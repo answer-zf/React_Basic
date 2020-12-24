3ba9fd7d17eecfd605a0344b39396d4aa73f7cf2
zhoufeng <657829956@qq.com>
2020-12-24 10:37:31 +0800

    unsave

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import LifeCyclePage from './pages/LifeCyclePage';
+// import ClassComponent from './pages/ClassComponent';
+// import FunctionComponent from './pages/FunctionComponent';
+// import SetStatePage from './pages/SetStatePage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			{/* 组件 */}
+			{/* <ClassComponent /> */}
+			{/* <FunctionComponent /> */}
+
+			{/* setState */}
+			{/* <SetStatePage /> */}
+
+			{/* 生命周期 */}
+			<LifeCyclePage />
+		</div>
+	);
 }
 
 export default App;
